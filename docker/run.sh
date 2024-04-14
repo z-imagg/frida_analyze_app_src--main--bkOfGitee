db1cad49ccff3fb408d9cdb5fb813343c312bc63
prgrmz07 <prgrmz07@163.com>
2024-04-14 16:29:40 +0800

    docker镜像构建过程中使用本地域名   add-host

--- docker/run.sh
+++ docker/run.sh
@@ -10,7 +10,7 @@ declare -r f=$(readlink -f ${BASH_SOURCE[0]})  ; declare -r d=$(dirname $f)
 cd $d
 
 #构建基础镜像
-docker images -q  --filter "reference=base_ubuntu_22.04"  ||  docker build  --no-cache  -f "./base_ubuntu_22_Dockerfile" -t base_ubuntu_22.04:0.1 "./" 
+docker images -q  --filter "reference=base_ubuntu_22.04"  ||  docker build --no-cache  -f "./base_ubuntu_22_Dockerfile" -t base_ubuntu_22.04:0.1 "./" 
 
 #删除现有 frida_anlz_ap镜像
 instanceIdLs=$(docker ps -a  -q --filter "ancestor=frida_anlz_ap:0.1")
@@ -29,8 +29,10 @@ imageIdLs=$(docker images -q  --filter "reference=frida_anlz_ap")
 
 
 #构建 frida_anlz_ap镜像
+# docker镜像构建过程中使用本地域名   add-host=giteaz:10.0.4.9 供给Dockerfile使用
+#      供给Dockerfile使用 == '构建过程中使用 而非docker实例运行过程中使用'
 # --pull  --rm
-docker build  --no-cache  -f "./Dockerfile" -t frida_anlz_ap:0.1 "./" 
+docker build --add-host=giteaz:10.0.4.9    --no-cache  -f "./Dockerfile" -t frida_anlz_ap:0.1 "./" 
 
 #启动 frida_anlz_ap镜像
 #  --rm 
