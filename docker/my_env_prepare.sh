c9f90915752a52d74a459451dfce05fc902881f7
prgrmz07 <prgrmz07@163.com>
2024-04-14 21:47:11 +0800

    # 若在docker下 根目录为 /tmp/ROOT/ 否则 根目录为 /， 保存 my_env_prepare.sh 执行结果目录

--- docker/my_env_prepare.sh
+++ docker/my_env_prepare.sh
@@ -15,6 +15,12 @@ chmod +x download_unpack.sh
 declare -r f=$(readlink -f ${BASH_SOURCE[0]})  ; declare -r d=$(dirname $f)
 cd $d
 
+# #region cytoscape 运行在宿主机上
+rootFsType=$(findmnt -n -o FSTYPE /)
+#若根目录挂载的文件系统类型为overlay, 则当前极有可能在docker下
+inDocker=[[ "$rootFsType" == "overlay" ]]
+
+
 # #endregion
 
 
@@ -28,17 +34,20 @@ LocalFileWebSrv=http://172.17.0.1:2111
 # #endregion
 
 # #region 业务体
+# 若在docker下 根目录为 /tmp/ROOT/ 否则 根目录为 /
+{ $inDocker && { RT=/tmp/ROOT/ && mkdir -p $RT ;} ;} || RT="/"
+
 
 echo "welcome to my_env_prepare"
 
 
 # #region nvm克隆、nvm函数导入、nvm安装nodejs-v18.19.1
 #nvm克隆
-git clone --branch=v0.39.7   https://gitee.com/repok/nvm-sh--nvm.git  /app/nvm
+git clone --branch=v0.39.7   https://gitee.com/repok/nvm-sh--nvm.git  $RT/app/nvm
 
 #nvm函数导入
 set +x 
-source /app/nvm/nvm.sh
+source $RT/app/nvm/nvm.sh
 set -x
 
 #nvm安装nodejs-v18.19.1
@@ -54,32 +63,30 @@ NVM_NODEJS_ORG_MIRROR=http://nodejs.org/dist nvm install v18.19.1
 
 
 #miniconda3
-F="Miniconda3-py310_22.11.1-1-Linux-x86_64.sh" ; ./download_unpack.sh https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/$F e01420f221a7c4c6cde57d8ae61d24b5  $F /app/pack/ /app/  $LocalFileWebSrv/$F ; unset F
+F="Miniconda3-py310_22.11.1-1-Linux-x86_64.sh" ; ./download_unpack.sh https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/$F e01420f221a7c4c6cde57d8ae61d24b5  $F $RT/app/pack/ $RT/app/  $LocalFileWebSrv/$F ; unset F
 
 #neo4j-4.4.32
-F="neo4j-community-4.4.32-unix.tar.gz" ; ./download_unpack.sh https://neo4j.com/artifact.php?name=$F a88d5de65332d9a5acbe131f60893b55  $F /app/pack/ /app/  $LocalFileWebSrv/$F ; unset F
+F="neo4j-community-4.4.32-unix.tar.gz" ; ./download_unpack.sh https://neo4j.com/artifact.php?name=$F a88d5de65332d9a5acbe131f60893b55  $F $RT/app/pack/ $RT/app/  $LocalFileWebSrv/$F ; unset F
 
 #neo4j-4.4.32需要的jdk11
-F="zulu11.70.15-ca-jdk11.0.22-linux_x64.tar.gz" ; ./download_unpack.sh https://cdn.azul.com/zulu/bin/$F f13d179f8e1428a3f0f135a42b9fa75b  $F /app/pack/ /app/  $LocalFileWebSrv/$F ; unset F
+F="zulu11.70.15-ca-jdk11.0.22-linux_x64.tar.gz" ; ./download_unpack.sh https://cdn.azul.com/zulu/bin/$F f13d179f8e1428a3f0f135a42b9fa75b  $F $RT/app/pack/ $RT/app/  $LocalFileWebSrv/$F ; unset F
 
 #neo4j安装apoc插件
-F="apoc-4.4.0.26-all.jar" ; ./download_unpack.sh https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.4.0.26/$F 5a42a32e12432632124acd682382c91d  $F /app/pack/ /app/  $LocalFileWebSrv/$F ; unset F
+F="apoc-4.4.0.26-all.jar" ; ./download_unpack.sh https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.4.0.26/$F 5a42a32e12432632124acd682382c91d  $F $RT/app/pack/ $RT/app/  $LocalFileWebSrv/$F ; unset F
 
 # #endregion
 
-# #region cytoscape 运行在宿主机上
-rootFsType=$(findmnt -n -o FSTYPE /)
-#若根目录挂载的文件系统类型为overlay, 则当前极有可能在docker下。 docker下 不安装cytoscape，因为很麻烦
-[[ "$rootFsType" == "overlay" ]] || { \
+#若docker下 不安装cytoscape，因为很麻烦
+$inDocker || { \
 
 # #region 物理机中安装 cytoscape
 # https://github.com/cytoscape/cytoscape/releases/tag/3.10.2
 
 # cytoscape-3.10.2
-F="cytoscape-unix-3.10.2.tar.gz" ; ./download_unpack.sh https://github.com/cytoscape/cytoscape/releases/download/3.10.2/$F a6b5638319b301bd25e0e6987b3e35fd  $F /app/pack/ /app/  $LocalFileWebSrv/$F ; unset F
+F="cytoscape-unix-3.10.2.tar.gz" ; ./download_unpack.sh https://github.com/cytoscape/cytoscape/releases/download/3.10.2/$F a6b5638319b301bd25e0e6987b3e35fd  $F $RT/app/pack/ $RT/app/  $LocalFileWebSrv/$F ; unset F
 
 # cytoscape-3.10.2需要的jdk17
-F="zulu17.48.15-ca-jdk17.0.10-linux_x64.tar.gz" ; ./download_unpack.sh https://cdn.azul.com/zulu/bin/$F bb826d2598b6ceaaae56a6c938f2030e  $F /app/pack/ /app/  $LocalFileWebSrv/$F ; unset F
+F="zulu17.48.15-ca-jdk17.0.10-linux_x64.tar.gz" ; ./download_unpack.sh https://cdn.azul.com/zulu/bin/$F bb826d2598b6ceaaae56a6c938f2030e  $F $RT/app/pack/ $RT/app/  $LocalFileWebSrv/$F ; unset F
 
 # #endregion
 
@@ -91,14 +98,14 @@ F="zulu17.48.15-ca-jdk17.0.10-linux_x64.tar.gz" ; ./download_unpack.sh https://c
 
 #miniconda3
 set +x 
-source /app/Miniconda3-py310_22.11.1-1/bin/activate
+source $RT/app/Miniconda3-py310_22.11.1-1/bin/activate
 set -x
 
 #jdk11 
-export JAVA_HOME=/app/zulu11.70.15-ca-jdk11.0.22-linux_x64
+export JAVA_HOME=$RT/app/zulu11.70.15-ca-jdk11.0.22-linux_x64
 
 #neo4j-4.4.32
-export NEO4J_HOME=/app/neo4j-community-4.4.32
+export NEO4J_HOME=$RT/app/neo4j-community-4.4.32
 
 export PATH=$PATH:$NEO4J_HOME/bin:$JAVA_HOME/bin
 
@@ -146,22 +153,23 @@ neo4j start
 echo "10.0.4.9 westgw giteaz g" | tee -a /etc/hosts
 
 #本项目fridaAnlzAp 代码拉取
-mkdir /fridaAnlzAp/
-git clone http://giteaz:3000/frida_analyze_app_src/main.git  /fridaAnlzAp/main
+mkdir $RT/fridaAnlzAp/
+git clone http://giteaz:3000/frida_analyze_app_src/main.git  $RT/fridaAnlzAp/main
 #递归拉取所有子模块
-( cd /fridaAnlzAp/main &&  git submodule    update --recursive --init )
+( cd $RT/fridaAnlzAp/main &&  git submodule    update --recursive --init )
 
+cd $RT/fridaAnlzAp/
 #删除软链接
-find  /fridaAnlzAp/  -maxdepth 1 -type l | xargs -I% unlink %
+find  .  -maxdepth 1 -type l | xargs -I% unlink %
 #直接子模块 或 app 链接到 上层目录
 # /fridaAnlzAp/x --->  /fridaAnlzAp/main/x
-ln -s  /fridaAnlzAp/main/analyze_by_graph  /fridaAnlzAp/analyze_by_graph
-ln -s /fridaAnlzAp/main/cmd-wrap  /fridaAnlzAp/cmd-wrap
-ln -s /fridaAnlzAp/main/frida_js   /fridaAnlzAp/frida_js
-ln -s  /fridaAnlzAp/main/github-gitee-GITEA   /fridaAnlzAp/github-gitee-GITEA
+ln -s  ./main/analyze_by_graph  ./analyze_by_graph
+ln -s ./main/cmd-wrap  ./cmd-wrap
+ln -s ./main/frida_js   ./frida_js
+ln -s  ./main/github-gitee-GITEA   ./github-gitee-GITEA
 # /fridaAnlzAp/x --->  /fridaAnlzAp/main/app/x
-ln -s /fridaAnlzAp/main/app/cgsecurity--testdisk   /fridaAnlzAp/cgsecurity--testdisk
-ln -s /fridaAnlzAp/main/app/torch-cpp  /fridaAnlzAp/torch-cpp
+ln -s ./main/app/cgsecurity--testdisk   ./cgsecurity--testdisk
+ln -s ./main/app/torch-cpp  ./torch-cpp
 
 
 # #endregion
@@ -170,13 +178,13 @@ ln -s /fridaAnlzAp/main/app/torch-cpp  /fridaAnlzAp/torch-cpp
 # #region 项目依赖安装
 #{python依赖安装
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-pip install -r /fridaAnlzAp/cmd-wrap/requirements.txt
-pip install -r /fridaAnlzAp/frida_js/requirements.txt
-pip install -r /fridaAnlzAp/analyze_by_graph/requirements.txt
+pip install -r $RT/fridaAnlzAp/cmd-wrap/requirements.txt
+pip install -r $RT/fridaAnlzAp/frida_js/requirements.txt
+pip install -r $RT/fridaAnlzAp/analyze_by_graph/requirements.txt
 #}
 
 #{nodejs依赖安装
-(cd /fridaAnlzAp/frida_js/ && npm install )
+(cd $RT/fridaAnlzAp/frida_js/ && npm install )
 #}
 
 # #endregion
