5a0945cea76a4c4b63fd587e1148484feb349799
prgrmz07 <prgrmz07@163.com>
2024-04-14 13:22:49 +0800

    直接子模块 或 app 链接到 上层目录, 草稿中

--- docker/my_env_prepare.sh
+++ docker/my_env_prepare.sh
@@ -26,6 +26,30 @@ echo "10.0.4.9 westgw giteaz g" | tee -a /etc/hosts
 mkdir /fridaAnlzAp/
 git clone http://giteaz:3000/frida_analyze_app_src/main.git  /fridaAnlzAp/main
 ( cd /fridaAnlzAp/main &&  git submodule    update --recursive --init )
+
+#直接子模块 链接到 上层目录
+# /fridaAnlzAp/x --->  /fridaAnlzAp/main/x
+# /fridaAnlzAp/main/analyze_by_graph --> /fridaAnlzAp/analyze_by_graph
+# /fridaAnlzAp/main/cmd-wrap --> /fridaAnlzAp/cmd-wrap
+# /fridaAnlzAp/main/frida_js --> /fridaAnlzAp/frida_js
+# /fridaAnlzAp/main/github-gitee-GITEA --> /fridaAnlzAp/github-gitee-GITEA
+
+# /fridaAnlzAp/main/app/cgsecurity--testdisk --> /fridaAnlzAp/cgsecurity--testdisk
+# /fridaAnlzAp/main/app/torch-cpp --> /fridaAnlzAp/torch-cpp
+git submodule foreach --recursive '
+  #因为pwd已经是子模块目录,不需要再加sm_path了, realDir=$(pwd)/$sm_path
+  realDir=$(pwd)  #子模块目录
+  if  case $sm_path in "app/"*"/"*) true;;     *) false;; esac; then
+  echo "【忽略app的子模块】$realDir "
+elif case $sm_path in "app/"*) true;;     *) false;; esac; then
+  dst="/fridaAnlzAp/$(basename $sm_path)" 
+  unlink  $dst 2>/dev/null ; { ln -s   $realDir $dst && echo "【软链接 app/子模块】 $realDir --> $dst" ;}
+else
+  dst="/fridaAnlzAp/$sm_path" 
+  unlink  $dst  2>/dev/null ; { ln -s  $realDir $dst && echo "【软链接 子模块】 $realDir --> $dst" ;}
+fi
+'
+
 # }
 
 #{python依赖安装
