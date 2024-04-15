82983eb0e46c669c6ec133a39d7773800d17d6f0
prgrmz07 <prgrmz07@163.com>
2024-04-15 19:44:33 +0800

    添加本地gitea域名

--- docker/docker_entry.sh
+++ docker/docker_entry.sh
@@ -5,7 +5,11 @@
 #【术语】 
 #【备注】   
 
+#此脚本任何语句 退出代码不为正常值0 ，都会导致整个脚本退出
+set -e
 
+#添加本地gitea域名
+grep "giteaz" /etc/hosts ||  ( echo "10.0.4.9 westgw giteaz g" | tee -a /etc/hosts || true )
 
 cd /fridaAnlzAp/
 /usr/bin/bash
