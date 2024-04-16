355310a773c75aacbb239bbcbb524f93f04cf585
prgrmz07 <prgrmz07@163.com>
2024-04-16 18:18:00 +0800

    docker实例运行的时候，才克隆项目代码，方便docker image上传到dockerhub ，同时也不泄漏项目源码

--- docker/docker_entry.sh
+++ docker/docker_entry.sh
@@ -13,7 +13,13 @@ source /fridaAnlzAp/main/docker/local_domain.sh
 #添加本地gitea域名
 local_domain_set
 
+
+#docker实例运行的时候，才克隆项目代码，方便docker image上传到dockerhub ，同时也不泄漏项目源码
+[[ -f /fridaAnlzAp/main/.git/config ]] || bash /fridaAnlzAp/main/docker/init_proj.sh
+
+
 cd /fridaAnlzAp/
+
 /usr/bin/bash
 
 
---
+++ docker/init_proj.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/bash +x
+
+#【描述】  
+#【依赖】   
+#【术语】 
+#【备注】   
+#【运行环境】 
+#    1. 此脚本 被docker实例(运行ubuntu22.04) 执行
+#    2. 此脚本 在宿主机ubuntu22.04下运行
+
+#此脚本任何语句 退出代码不为正常值0 ，都会导致整个脚本退出
+set -e
+
+chmod +x /fridaAnlzAp/main/docker/*.sh
+
+source /fridaAnlzAp/main/docker/util.sh
+source /fridaAnlzAp/main/docker/local_domain.sh
+
+
+
+# #region 项目
+
+#   #region 项目代码拉取
+
+#本地gitea服务器. 当文件系统是只读时，tee可能会写入了 但还是报错， 因此放入'子进程()'中 避免干扰此脚本
+local_domain_set
+
+#本项目fridaAnlzAp 代码拉取
+mkdir -p /fridaAnlzAp/
+git clone http://giteaz:3000/frida_analyze_app_src/main.git  /fridaAnlzAp/main
+#git项目忽略文件权限变动
+( cd /fridaAnlzAp/main ; git_ignore_filemode ;)
+chmod +x /fridaAnlzAp/main/docker/*.sh
+#递归拉取所有子模块
+( cd /fridaAnlzAp/main &&  git submodule    update --recursive --init )
+
+cd /fridaAnlzAp/
+#删除软链接
+find  .  -maxdepth 1 -type l | xargs -I% unlink %
+#直接子模块 或 app 链接到 上层目录
+# /fridaAnlzAp/x --->  /fridaAnlzAp/main/x
+ln -s  ./main/analyze_by_graph  ./analyze_by_graph
+ln -s ./main/cmd-wrap  ./cmd-wrap
+ln -s ./main/frida_js   ./frida_js
+ln -s  ./main/github-gitee-GITEA   ./github-gitee-GITEA
+# /fridaAnlzAp/x --->  /fridaAnlzAp/main/app/x
+ln -s ./main/app/cgsecurity--testdisk   ./cgsecurity--testdisk
+ln -s ./main/app/torch-cpp  ./torch-cpp
+
+
+#   #endregion
+
+
+#   #region 项目依赖安装
+#{python依赖安装
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+pip install -r /fridaAnlzAp/cmd-wrap/requirements.txt
+pip install -r /fridaAnlzAp/frida_js/requirements.txt
+pip install -r /fridaAnlzAp/analyze_by_graph/requirements.txt
+#}
+
+ls -lh /
+
+#   #endregion
+
+# #endregion
