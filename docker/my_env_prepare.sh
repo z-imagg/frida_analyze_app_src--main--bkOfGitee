24262ff51a81cbf90d5e9d7366b1df147db5d094
prgrmz07 <prgrmz07@163.com>
2024-04-16 19:45:04 +0800

    草稿，Dockerfile与shell互转

--- docker/my_env_prepare.sh
+++ docker/my_env_prepare.sh
@@ -62,17 +62,7 @@ echo "welcome to my_env_prepare"
 
 #   #region nvm克隆、nvm函数导入、nvm安装nodejs-v18.19.1
 #nvm克隆
-git clone --branch=v0.39.7   https://gitee.com/repok/nvm-sh--nvm.git  /app/nvm
 
-#nvm函数导入
-source /app/nvm/nvm.sh 1>/dev/null 2>/dev/null
-
-#nvm安装nodejs-v18.19.1
-#  v18.19.1 是 nodejs  LTS v18 系列 中 最后一个版本
-export NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node/
-export NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node/
-# NVM_NODEJS_ORG_MIRROR=http://nodejs.org/dist nvm  ls-remote | grep v18. | grep LTS
-NVM_NODEJS_ORG_MIRROR=http://nodejs.org/dist nvm install v18.19.1  1>/dev/null 2>/dev/null
 #   #endregion
 
 #   #region 下载包 、 解压包 , miniconda3 、 neo4j-4.4.32 、 jdk11  、 neo4j的apoc插件
