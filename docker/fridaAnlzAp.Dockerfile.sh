2ba4bef31b2ff2521304b7f102a71d01371cc91e
prgrmz07 <prgrmz07@163.com>
2024-04-16 19:53:57 +0800

    草稿，Dockerfile与shell互转

--- docker/fridaAnlzAp.Dockerfile.sh
+++ docker/fridaAnlzAp.Dockerfile.sh
@@ -1,10 +1,32 @@
-#【备注】 
+#!/usr/bin/bash
+
+#【描述】  
+#【依赖】   
+#【术语】 
+#【备注】
 
 ##FROM base_ubuntu_22.04:0.1 as base
 
+#此脚本任何语句 退出代码不为正常值0 ，都会导致整个脚本退出
+##RUN \
+set -e && \
+shopt -s expand_aliases && \
+true
+
 ##WORKDIR /
 
-##COPY /fridaAnlzAp/main/docker /dockerBuildROOT/fridaAnlzAp/main/docker
+##COPY /fridaAnlzAp/main/docker /fridaAnlzAp/main/docker
+
+##RUN \
+mkdir -p /app/pack/ /app/ && \
+chmod +x /fridaAnlzAp/main/docker/*.sh && \
+source /fridaAnlzAp/main/docker/file_web_srv.sh && \
+source /fridaAnlzAp/main/docker/util.sh && \
+source /fridaAnlzAp/main/docker/local_domain.sh && \
+#判定当前 是在docker实例中 还是 在 宿主物理机 中 
+# 返回变量为 inDocker
+isInDocker && \
+true
 
 ##RUN \
 /dockerBuildROOT/fridaAnlzAp/main/docker/my_env_prepare.sh
--- docker/my_env_prepare.sh
+++ docker/my_env_prepare.sh
@@ -1,31 +1,10 @@
-#!/usr/bin/bash +x
 
-#【描述】  
-#【依赖】   
-#【术语】 
-#【备注】   
-#【运行环境】 
-#    1. 此脚本 被Dockerfile的RUN命令调用  , 则运行在 Ubuntu Jammy Jellyfish 下
-#    2. 此脚本 在宿主机ubuntu22.04下运行
 
-#此脚本任何语句 退出代码不为正常值0 ，都会导致整个脚本退出
-set -e
 
-mkdir -p /app/pack/ /app/
 
-# #region 基础头
-shopt -s expand_aliases
 
 #   #region 开发体
 
-chmod +x /dockerBuildROOT/fridaAnlzAp/main/docker/*.sh
-
-source /dockerBuildROOT/fridaAnlzAp/main/docker/file_web_srv.sh
-source /dockerBuildROOT/fridaAnlzAp/main/docker/util.sh
-source /dockerBuildROOT/fridaAnlzAp/main/docker/local_domain.sh
-
-#判定当前 是在docker实例中 还是 在 宿主物理机 中 
-isInDocker # 返回变量为 inDocker
 
 #开发用，本地文件下载web服务
 # 物理机下才 启动 本地文件下载web服务
