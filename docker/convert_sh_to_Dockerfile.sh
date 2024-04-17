50df5c8d42c1a0298cad805d1d6ef12b4b2d2ee9
prgrmz07 <prgrmz07@163.com>
2024-04-17 21:08:58 +0800

    13

--- docker/convert_sh_to_Dockerfile.sh
+++ docker/convert_sh_to_Dockerfile.sh
@@ -5,4 +5,8 @@
 #【术语】 
 #【备注】  
 
+#去此脚本所在目录
+declare -r f=$(readlink -f ${BASH_SOURCE[0]})  ; declare -r d=$(dirname $f)
+cd $d
+
 sed -e "s/#dk# //g"  -e  "s/#dk# '''//g"  -e   "s/#dksh# COPY/cp/g"  -e   "s/#dksh# WORKDIR/cd/g"  docker/fridaAnlzAp.Dockerfile.sh | tee docker/fridaAnlzAp.Dockerfile
