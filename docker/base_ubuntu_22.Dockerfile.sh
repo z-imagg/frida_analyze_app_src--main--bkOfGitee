581273aca8d606bb7e810a6c56a0f8ab23a45780
prgrmz07 <prgrmz07@163.com>
2024-04-17 22:41:38 +0800

    30

--- docker/base_ubuntu_22.Dockerfile.sh
+++ docker/base_ubuntu_22.Dockerfile.sh
@@ -1,13 +1,19 @@
-#FROM ubuntu:22.04 as base
 
+#【术语】 #dk# == #docker# == 仅docker有,  #sh# == #bash# == 仅bash有, #dksh# == #docker_shell# == docker有、bash有 
+#【备注】 在bash中 冒号':' 表示 空指令
 
-##RUN \
-apt-get update
+#dk# FROM ubuntu:22.04 as base
+#dk# WORKDIR /
 
-##RUN \
-apt-get install -y axel wget curl  net-tools git  iputils-ping python3 python3-urllib3 sudo
+#dk# RUN bash -c ''' \
+{ \
+apt-get update && \
+apt-get install -y axel wget curl  net-tools git  iputils-ping python3 python3-urllib3 sudo && \
+true ;} \
+|| false #dk# '''
 
-##RUN \
+#dk# RUN bash -c ''' \
+{ \
 mkdir -p /app/nvm && \
 #nvm克隆
 git clone --branch=v0.39.7   https://gitee.com/repok/nvm-sh--nvm.git  /app/nvm && \
@@ -18,4 +24,5 @@ export NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node/ && \
 export NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node/ && \
 # NVM_NODEJS_ORG_MIRROR=http://nodejs.org/dist nvm  ls-remote | grep v18. | grep LTS
 NVM_NODEJS_ORG_MIRROR=http://nodejs.org/dist nvm install v18.19.1  1>/dev/null 2>/dev/null && \
-true
+true ;} \
+|| false #dk# '''
